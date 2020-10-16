0951ce4c29d3f3699386e016e23115c7472e5a21
Yevhenii Khudolii <youginetravel@gmail.com>
2020-10-16 14:28:56 +0300

    Initial Commit

---
+++ Home Work 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Home Work 2
+//
+//  Created by Yevhenii on 16.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
