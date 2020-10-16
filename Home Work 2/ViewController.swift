3e653ecfd40e32eb44bbda4eddc88e60178e0260
Yevhenii Khudolii <youginetravel@gmail.com>
2020-10-16 14:36:42 +0300

    Assignment 1. Columns

--- Home Work 2/ViewController.swift
+++ Home Work 2/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor.white
+        
+        line(value: 3)
     }
-
-
+    
+    func drawBox(x: CGFloat, y: CGFloat, width: CGFloat, height: CGFloat) {
+        let box = UIView()
+        box.backgroundColor = UIColor.blue
+        box.frame = CGRect (x: x, y: y, width: width, height: height)
+        view.addSubview(box)
+    }
+    
+    func line (value: Int) {
+        let padding: CGFloat = 20
+        var xPos: CGFloat = 50
+        let boxSize: CGFloat = 50
+        for _ in 0..<value {
+            drawBox(x: xPos, y: 50, width: 50, height: 50)
+            xPos += padding + boxSize
+        }
+    }
 }
-
